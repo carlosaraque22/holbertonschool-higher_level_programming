b70b8e593d849ec1e37391153ddbca19c5821cba
Carlos Araque <cararqueu@gmail.com>
2020-12-08 15:36:47 +0000

    fix task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for lowercases in range(97, 123):
-    if lowercases in [101,113]:
+    if lowercases in (101,113):
         continue
     else:
         print("{:c}".format(lowercases), end="")
