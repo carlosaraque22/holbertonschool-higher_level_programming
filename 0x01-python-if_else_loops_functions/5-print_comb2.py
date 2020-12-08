77219ebf5615e3d36621510c6da451a622637180
Carlos Araque <cararqueu@gmail.com>
2020-12-08 16:16:17 +0000

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for numbers in range(0, 100):
-    print("{:02d},".format(numbers), end=' ')
+for numbers in range(0, 99):
+    print("{:02d}".format(numbers), end=', ')
+print("{:02d}".format(numbers + 1))
