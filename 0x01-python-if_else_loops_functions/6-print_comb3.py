2eb493a4607dc17e866dad55686d587e816d402e
Carlos Araque <cararqueu@gmail.com>
2020-12-08 16:34:55 +0000

    changes for task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for number in range(10):
     for num in range(number + 1, 10):
-        print("{}".format(str(number) + str(num)), end=", ")
+        print("{}".format(str(number) + str(num)), end="")
+        if int(str(number) + str(num)) < 89:
+            print(", ", end="")
