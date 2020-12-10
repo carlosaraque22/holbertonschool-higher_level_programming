3e0e637fb91b60632a0b3e7d5f2804056b47de7b
Carlos Araque <cararqueu@gmail.com>
2020-12-10 16:13:33 +0000

    changes for the task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if (len(sys.argv)) < 2:
     print("0 arguments.")
 else:
     if (len(sys.argv)) == 2:
-        print("{:d} argument:".format(len(sys.argv)))
+        print("{:d} argument:".format((len(sys.argv)) - 1))
     else:
-        print("{:d} arguments:".format(len(sys.argv)))
+        print("{:d} arguments:".format((len(sys.argv)) - 1))
     for string in range(1, (len(sys.argv))):
         print("{:d}: {}".format(string, sys.argv[string]))
