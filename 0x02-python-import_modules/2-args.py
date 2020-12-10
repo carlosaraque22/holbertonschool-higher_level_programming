72ae99e4547dcc9bf4ebf1555deb70506109e21f
Carlos Araque <cararqueu@gmail.com>
2020-12-10 16:10:43 +0000

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+if (len(sys.argv)) < 2:
+    print("0 arguments.")
+else:
+    if (len(sys.argv)) == 2:
+        print("{:d} argument:".format(len(sys.argv)))
+    else:
+        print("{:d} arguments:".format(len(sys.argv)))
+    for string in range(1, (len(sys.argv))):
+        print("{:d}: {}".format(string, sys.argv[string]))
