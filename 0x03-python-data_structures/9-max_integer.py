494df6a54c45b991f061510fa4f6d936537db867
Carlos Araque <cararqueu@gmail.com>
2020-12-15 21:36:33 +0000

    tasks 8 and 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+        if my_list:
+            s = my_list[0]
+            for a in range(1, len(my_list)):
+                if my_list[a] > s:
+                    s = my_list[a]
+            return s
+        else:
+            return None
