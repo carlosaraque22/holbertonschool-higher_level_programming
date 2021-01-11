db660f35083c99f380e6b90248bf1ed3bb42812c
Carlos Araque <cararqueu@gmail.com>
2021-01-11 23:14:54 +0000

    tasks 0, 1, and 3

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,12 @@
-#! /usr /bin /python3
+#!/usr/bin/python3
 """
 Project: 0x08. Python - More Classes and Objects
 Task: 0
 """
 
+
 class Rectangle:
-    pass
     """"
     Empty class Rectangle that defines a rectangle
     """
+    pass
