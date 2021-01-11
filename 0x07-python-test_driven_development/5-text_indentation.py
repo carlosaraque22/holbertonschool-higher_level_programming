80d9c08f4b9ae63a96d40579aa8ed740e46b4eed
Carlos Araque <cararqueu@gmail.com>
2021-01-11 02:57:14 +0000

    all tasks and tests completed

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def text_indentation(text):
+    """
+    prints a string of text with 2 new lines after '.', '?', and ':'
+    unit tests located in tests/5-text_indentation.txt
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    to_print = ""
+    characters = ['.', '?', ':']
+    for string in text:
+        to_print += string
+        if string in characters:
+            to_print += "\n\n"
+            print(to_print, end='')
