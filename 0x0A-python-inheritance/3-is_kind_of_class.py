6ee80a644117ef63a9feda714e705b8ab4468224
Carlos Araque <cararqueu@gmail.com>
2021-01-20 02:48:47 +0000

    fixing errors

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+Project: 0x0A-python-inheritance
+Task: 3
+"""
+
+
 def is_kind_of_class(obj, a_class):
+    """True if it is, False otherwise"""
+
     if issubclass(type(obj), a_class):
         return True
     return False
