fe70f2e69ac54c9b259ffec615b3837463320299
Carlos Araque <cararqueu@gmail.com>
2021-01-20 16:13:06 -0300

    fixing tasks

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-"""
-Project: 0x0A-python-inheritance.
-Task: 7
-"""
+"""Module with BaseGeometry class"""
 
 
 class BaseGeometry():
-    """
-    Based on 6-base_geometry.py
-    """
+    """Class with atribute public area"""
     def area(self):
-        """
-        Public instance method: def area(self):
-        that raises an Exception with the message area() is not implemented.
-        """
+        """Raise an exception when is called"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Public instance method: def integer_validator(self, name, value):
-        that validates value.
-        """
-        if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+        """Validates that 'value' is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python3
-"""class rectangle"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Module with BaseGeometry class"""
+
+
+class BaseGeometry():
+    """Class with atribute public area"""
+    pass
+    """Raise an exception when is called"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates that 'value' is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
-    """creates base height and width of the rectangle
-    Args:
-        BaseGeometry: inheriting class BaseGeometry
-    """
+    """"Representation of a rectangule"""
     def __init__(self, width, height):
-        """Instantiation rectaclge with width and height
-        Args:
-            width: size of width
-            height: size of height
-        Attributes:
-            __width: width of the rectangle
-            __height: height of the rectangle
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """Instantiation of a rectangle"""
+        self.integer_validator("with", width)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """calculate the area of rectangle
-        Return:
-            area of the rectangle
-        """
+        """Returns the area of the rectangle"""
         return self.__width * self.__height
 
     def __str__(self):
-        """return a string of the area funciton
-        Return:
-            returns a string
-        """
+        """Print and return rectangle description"""
         return ("[Rectangle] {}/{}".format(self.__width, self.__height))
