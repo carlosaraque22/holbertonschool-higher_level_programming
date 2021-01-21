c75d9147de48acb3817040e927bf4df2352c0330
Carlos Araque <cararqueu@gmail.com>
2021-01-20 23:26:56 -0300

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Class description and import JSON"""
+
+
+def from_json_string(my_str):
+    """ Function to convert a JSON string to python object """
+    y = json.loads(my_str)
+    return y
