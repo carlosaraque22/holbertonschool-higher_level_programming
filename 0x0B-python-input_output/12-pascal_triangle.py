503cc1b71added3f10c65f6bdf6c1fd52268d26b
Carlos Araque <cararqueu@gmail.com>
2021-01-21 13:50:31 -0300

    uwu

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-""" Class description """
+""" Class Rectangle """
 
 
 def pascal_triangle(n):
-    """
-    Create a function def pascal_triangle(n):
-    that returns a list of lists of integers representing
-    the Pascals triangle of n
-    """
-    new_list = []
-    if n <= 0:
-        return new_list
-    new_list = [[1], [1, 1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(0, len(new_list[i]) - 1):
-            row.extend([new_list[i][j] + new_list[i][j+1]])
-        row += [1]
-        new_list.append(row)
-    return new_list
+    """Representation of pascal triangle 'n'"""
+    triangle = []
+    if n > 0:
+        triangle.append([1])
+        if n > 1:
+            for i in range(1, n):
+                row = [1]
+                for j in range(1, i):
+                    row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+                row.append(1)
+                triangle.append(row)
+    return triangle
