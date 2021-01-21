4a7c1eb67927462ce40f4be8e2eae962c8e05f80
Carlos Araque <cararqueu@gmail.com>
2021-01-21 01:43:22 -0300

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Class description and import JSON """
+"""Class description and import JSON."""
 from sys import argv
 import json
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+create_json = __import__('6-load_from_json_file').load_from_json_file
+save_json = __import__('5-save_to_json_file').save_to_json_file
 
 
 try:
-    new_list = load_from_json_file("add_item.json")
+    new_list = create_json("add_item.json")
 except FileNotFoundError:
     new_list = []
 for item in range(1, len(argv)):
     new_list.append(argv[item])
-save_to_json_file(new_list, "add_item.json")
+save_json(new_list, "add_item.json")
