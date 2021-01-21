c71477d4c3bdfde300750a0062847b7c756a72af
Carlos Araque <cararqueu@gmail.com>
2021-01-20 22:36:00 -0300

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Class description """
+
+
+def append_write(filename="", text=""):
+    """
+    Function that appends a string at the end of 
+    a text file (UTF8)
+    """
+    with open(filename, mode="a", encoding="utf-8") as my_file
+        return my_file.write(text)
