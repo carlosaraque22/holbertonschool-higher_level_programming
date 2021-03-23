05d89f4c90c9c72342df3d29f306dfd2d5e71818
Carlos Araque <cararqueu@gmail.com>
2021-03-23 23:11:24 +0000

    lasts tasks

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-
-const newlist = list.map(x => x * 2);
+const newlist = list.map((x, index) => x * index);
 console.log(list);
 console.log(newlist);
