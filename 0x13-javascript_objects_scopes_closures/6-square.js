53be8ece521ff32ef3b508e26a4ed2a5e5ef15e3
Carlos Araque <cararqueu@gmail.com>
2021-03-23 18:48:46 +0000

    fix task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,13 @@
 const Parent = require('./5-square');
 class Square extends Parent {
   charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
     let rectangle = '';
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        rectangle += c !== undefined ? c : 'X';
+        rectangle += c;
       }
       if (i < this.height - 1) {
         rectangle += '\n';
